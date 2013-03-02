8222a6a1a8b816039dee4aa7a22424cfc8fd46fd
Wayne L. Kille <wayne_kille@hotmail.com>
2013-03-02 13:28:28 +0000

    Refactor basic static page tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,27 +4,45 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Turnip'" do
+    it "should have the h1 'Turnip'" do
       visit '/static_pages/home'
-      page.should have_content('Turnip')
+      page.should have_selector('h1', text: 'Turnip')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "Turnip | Home")
+      page.should have_selector('title', text: "Turnip")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', text: '| Home')
     end
   end
 
   describe "About page" do
 
-    it "should have the content 'About'" do
+    it "should have the h1 'About'" do
       visit '/static_pages/about'
-      page.should have_content('About')
+      page.should have_selector('h1', text: 'About')
     end
 
     it "should have the title 'About'" do
       visit '/static_pages/about'
-      page.should have_selector('title', :text => "Turnip | About")
+      page.should have_selector('title', text: "Turnip | About")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', text: "Turnip | Contact")
     end
   end
 end
