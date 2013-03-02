15264ca57fd1aac6dbeab02726972419be8c0d1b
Wayne L. Kille <wayne_kille@hotmail.com>
2013-03-02 18:59:08 +0000

    Improve layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,47 +2,30 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the h1 'Turnip'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', text: 'Turnip')
-    end
+    it { should have_selector('h1', text: 'Turnip') }
+    it { should have_selector('title', text: 'Turnip') }    
+    it { should_not have_selector('title', text: '| Home') }
 
-    it "should have the base title" do
-      visit '/static_pages/home'
-      page.should have_selector('title', text: "Turnip")
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', text: '| Home')
-    end
   end
 
   describe "About page" do
+    before { visit about_path }
 
-    it "should have the h1 'About'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', text: 'About')
-    end
-
-    it "should have the title 'About'" do
-      visit '/static_pages/about'
-      page.should have_selector('title', text: "Turnip | About")
-    end
+    it { should have_selector('h1', text: 'About') }
+    it { should have_selector('title', text: "Turnip | About") }  
+  
   end
 
   describe "Contact page" do
+    before { visit contact_path }
 
-    it "should have the h1 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', text: 'Contact')
-    end
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_selector('title', text: "Turnip | Contact") }  
 
-    it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('title', text: "Turnip | Contact")
-    end
   end
 end
