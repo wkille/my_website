65299961ae2f676059c2abfcd614c6cf4eb9f9e7
Wayne L. Kille <wayne_kille@hotmail.com>
2013-03-03 20:27:41 +0000

    Create product and prices pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,10 @@ class StaticPagesController < ApplicationController
 
   def contact
   end
+
+  def product
+  end
+
+  def prices
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ MyWebsite::Application.routes.draw do
   root to: 'static_pages#home'
   match '/about',   to: 'static_pages#about'
   match '/contact',   to: 'static_pages#contact'
+  match '/product',   to: 'static_pages#product'
+  match '/prices',   to: 'static_pages#prices'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
